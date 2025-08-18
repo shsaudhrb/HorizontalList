204acdb99d20c7691588a1a6b9ecfb2eaf4f9031
ETHAR\NTG Clarity <ealrehaili@ntgclarity.com>
2025-08-18 16:41:51 +0300

    VLA-1 setup core infrastructure

--- app/src/main/java/com/example/lmd/MainActivity.kt
+++ app/src/main/java/com/example/lmd/MainActivity.kt
@@ -4,13 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.lmd.ui.theme.LmdTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,32 +20,23 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             LmdTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding),
-                    )
-                }
+                Greeting()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(
-    name: String,
-    modifier: Modifier = Modifier,
-) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier,
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LmdTheme {
-        Greeting("Android")
+fun Greeting() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "This is LMD",
+            color = MaterialTheme.colorScheme.onBackground
+        )
     }
 }
