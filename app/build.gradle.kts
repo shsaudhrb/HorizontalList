9bbd17eab3f4f76ec1980f609a062115d0370686
LAPTOP-1CR9DKRB\Renad <rsalah@ntgclarity.com>
2025-08-21 14:40:50 +0300

    VLA-11 notification list UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     alias(libs.plugins.kotlin.compose)
     id("org.jlleitschuh.gradle.ktlint") version "13.0.0"
     id("io.gitlab.arturbosch.detekt") version "1.23.8"
+    alias(libs.plugins.google.gms.google.services)
 }
 android {
     namespace = "com.ntg.lmd"
@@ -78,6 +79,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.firebase.messaging)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
