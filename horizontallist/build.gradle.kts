e16bbc09b2e5b8751a31729d423ec4bd00e2225c
Renad\NTG Clarity <shsaud@ntgclarity.com>
2025-09-22 11:30:31 +0300

    VLA-89 library for Horizontal list

--- horizontallist/build.gradle.kts
+++ horizontallist/build.gradle.kts
@@ -47,7 +47,7 @@ afterEvaluate {
             create<MavenPublication>("release") {
                 from(components["release"])
 
-                groupId = "com.github.ntglms"
+                groupId = "com.bitbucket.ntglms"
                 artifactId = "horizontallist"
                 version = "1.0.0"
             }
